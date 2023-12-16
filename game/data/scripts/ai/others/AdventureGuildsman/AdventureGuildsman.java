d86b8ce23a6681569355a84788dca1d1495a1dc5
FeralHorse <pusiket90@yandex.ru>
2023-12-16 12:34:05 +0300

    Переведены тексты члена гильдии путешественника

--- game/data/scripts/ai/others/AdventureGuildsman/AdventureGuildsman.java
+++ game/data/scripts/ai/others/AdventureGuildsman/AdventureGuildsman.java
@@ -16,10 +16,7 @@
  */
 package ai.others.AdventureGuildsman;
 
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
+import ai.AbstractNpcAI;
 import org.l2jmobius.gameserver.data.xml.MultisellData;
 import org.l2jmobius.gameserver.enums.CategoryType;
 import org.l2jmobius.gameserver.model.actor.Npc;
@@ -29,10 +26,12 @@ import org.l2jmobius.gameserver.model.skill.Skill;
 import org.l2jmobius.gameserver.model.skill.SkillCaster;
 import org.l2jmobius.gameserver.network.serverpackets.ExShowQuestInfo;
 
-import ai.AbstractNpcAI;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
- * Adventure Guildsman AI.
+ * AdventureGuildsman AI.
  * @author ChaosPaladin
  */
 public class AdventureGuildsman extends AbstractNpcAI
