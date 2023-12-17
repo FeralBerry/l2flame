ef3b9fd5042ad89936c1c8d1d987c148b5e84d58
FeralHorse <pusiket90@yandex.ru>
2023-12-17 09:07:51 +0300

    добавлена заготовка для квеста на 1 профессию. Изменен тип полчения професси для получения профессии нужно иметь определнный итем в инвентаре

--- game/data/scripts/quests/Q00008_HelpingAnElfToExplore/Q00008_HelpingAnElfToExplore.java
+++ game/data/scripts/quests/Q00008_HelpingAnElfToExplore/Q00008_HelpingAnElfToExplore.java
@@ -1,4 +1,10 @@
 package quests.Q00008_HelpingAnElfToExplore;
 
-public class Q00008_HelpingAnElfToExplore {
+import org.l2jmobius.gameserver.model.quest.Quest;
+
+public class Q00008_HelpingAnElfToExplore extends Quest {
+    private static final int QUEST_ID = 8;
+    public Q00008_HelpingAnElfToExplore(){
+        super(QUEST_ID);
+    }
 }
--- game/data/scripts/quests/Q00009_ZombieHunting/Q00009_ZombieHunting.java
+++ game/data/scripts/quests/Q00009_ZombieHunting/Q00009_ZombieHunting.java
@@ -1,4 +1,10 @@
 package quests.Q00009_ZombieHunting;
 
-public class Q00009_ZombieHunting {
+import org.l2jmobius.gameserver.model.quest.Quest;
+
+public class Q00009_ZombieHunting extends Quest {
+    private static final int QUEST_ID = 9;
+    public Q00009_ZombieHunting(){
+        super(QUEST_ID);
+    }
 }
